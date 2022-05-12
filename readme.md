07d9b8eff2198435f3f5c48100d24dfe42be5171
PUF <PUF@DESKTOP-P2TFS0N>
2022-05-13 00:28:11 +0300

    initial commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+
+# First Project
+## A git demonstration
+
+Andreia Pantiruc
+
+andreia.pantiruc@student.tuiasi.ro
+
+This repository is hosted at:
+
+https://github.com/andreiapantiruc/Task1Pip.git
---
+++ src/FirstPackage1/FirstClass1.java
@@ -0,0 +1,12 @@
+package FirstPackage1;
+
+public class FirstClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("First text");
+		
+	}
+
+}
