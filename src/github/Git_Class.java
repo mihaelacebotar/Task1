f2d8ca867955d2bb36a18b4674af98b6c5cefd21
Mihaela <Mihaela@DESKTOP-HAMJB5E>
2022-05-12 19:02:47 +0300

    added a nice question

--- src/github/Git_Class.java
+++ src/github/Git_Class.java
@@ -6,6 +6,8 @@ public class Git_Class {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Hello World!");
+		System.out.println("How are you?");
+		System.out.println("I'm fine, thank you!");
 		
 
 	}
